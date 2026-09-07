193197d51ba5dcf4d37ba4f31ccb6fb1ffbbafc4
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-07 17:12:06 +0330

    Change permission to IsAuthenticated

--- apps/users/serializers.py
+++ apps/users/serializers.py
@@ -11,7 +11,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'first_name',
-            'last_name', 'email',
-            'is_active', 'created_at', 'updated_at'
+            'last_name', 'email', 'is_active',
+            'created_at', 'updated_at'
         ]
         read_only_filed = ['is_active']
