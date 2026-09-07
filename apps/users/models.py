948607ac0769493c192d0276eb18e1353255c666
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-07 11:09:21 +0330

    fix01

--- apps/users/models.py
+++ apps/users/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import (
 )
 from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.http import http404
+from django.http import Http404
 
 
 # ================================
