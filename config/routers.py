8ffb67bd3aebfe4c0a161bffa0c8b408090243c8
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-07 18:10:37 +0330

    Register User Endpoint Created and Added

--- config/routers.py
+++ config/routers.py
@@ -3,11 +3,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from apps.users.viewsets import UserViewSet
+from apps.auth.viewsets.register import RegisterViewSet
 
 
 router = DefaultRouter()
 
 router.register(r'users', UserViewSet, basename='user')
+router.register(r'auth/register', RegisterViewSet, basename='auth-register')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
