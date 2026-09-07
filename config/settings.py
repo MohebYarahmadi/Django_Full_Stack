694a61c56645e2a312e58a649d91424c9c45f3ff
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-07 17:35:15 +0330

    SimpleJWT installed and Auth app added

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # External apps
     'rest_framework',
+    'rest_framework_simplejwt',
 
     # My apps
     'apps.users.apps.UsersConfig',
@@ -133,3 +134,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS':
+    ['django_filters.rest_framework.DjangoFilterBackend'],
+}
