577800ad6756c57cba4fa4507d5f5ac0d8d99016
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-09 18:40:24 +0330

    Adding likes, remove and chech for like

--- apps/users/models.py
+++ apps/users/models.py
@@ -72,6 +72,18 @@ class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     def name(self):
         return f'{self.first_name} {self.last_name}'
 
+    def like(self, post):
+        """Like `post` if it hasn't been done yet"""
+        return self.posts_liked.add(post)
+
+    def remove_like(self, post):
+        """Remove a like from a `post`"""
+        return self.posts_liked.remove(post)
+
+    def has_liked(self, post):
+        """Return True if the user has liked a `post`; else Flase"""
+        return self.posts_liked.filter(pk=post.pk).exists()
+
     def __str__(self):
         return f'{self.email}'
 # ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
