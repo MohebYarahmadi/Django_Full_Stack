4898f1727c2bb92b64dccd1d8d8b0064de5af5a4
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-09 10:39:53 +0330

    Post Endpoints Test Done for GET,POST

--- apps/posts/viewsets.py
+++ apps/posts/viewsets.py
@@ -1,4 +1,6 @@
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework import status
 
 from apps.abstracts.viewsets import AbstractViewSet
 from apps.posts.models import Post
