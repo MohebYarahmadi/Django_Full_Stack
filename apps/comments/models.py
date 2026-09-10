f91f1292939a66a59b2a2fc7f457636872916457
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-10 10:34:04 +0330

    Comments model created

--- apps/comments/models.py
+++ apps/comments/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+from apps.abstracts.models import AbstractModel, AbstractManager
+
+
+class CommentManager(AbstractManager):
+    pass
+
+
+class Comment(AbstractModel):
+    post = models.ForeignKey('apps_posts.Post', on_delete=models.PROTECT)
+    author = models.ForeignKey('apps_users.User', on_delete=models.PROTECT)
+
+    body = models.TextField()
+    edited = models.BooleanField(default=False)
+
+    objects = CommentManager()
+
+    class Meta:
+        db_table = "apps.comments"
+
+    def __str__(self):
+        return self.author.name
