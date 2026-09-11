93b739b03a71f2f10b678c4290dce6e569ad1262
Moheb Yarahmadi <moheb.yarahmadi@gmail.com>
2026-09-11 18:13:19 +0330

    Write model test and fixtures for User and Post

---
+++ apps/fixtures/users_fixture.py
@@ -0,0 +1,15 @@
+import pytest
+from apps.users.models import User
+
+
+data_user = {
+    'username': 'test_use',
+    'email': 'test@gmail.com',
+    'first_name': 'Test',
+    'last_name': 'User',
+    'password': 'test_password'
+}
+
+@pytest.fixture
+def user(db) -> User:
+    return User.objects.create_user(**data_user)
